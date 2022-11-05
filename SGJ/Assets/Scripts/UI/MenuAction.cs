cf9273e80ef6e13eaf3f82b311105aaf4f169453
TwilightHunt <gangreet12@gmail.com>
2022-11-05 15:16:10 +0300

    fix: on deselect keyboard move

---
+++ SGJ/Assets/Scripts/UI/MenuAction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MenuAction : MonoBehaviour
+{
+    [SerializeField] Button defaultButton;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (EventSystem.current.currentSelectedGameObject == null) { defaultButton.Select(); }
+    }
+}
