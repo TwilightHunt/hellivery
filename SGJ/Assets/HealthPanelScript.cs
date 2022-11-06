3019ce48db876d50d08f3bf08791393434a1b557
Евгений <koronazheka@gmail.com>
2022-11-06 17:17:53 +0300

    Puch

--- SGJ/Assets/HealthPanelScript.cs
+++ SGJ/Assets/HealthPanelScript.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuguFloat : MonoBehaviour
+public class HealthPanelScript : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +13,6 @@ public class FuguFloat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        
     }
 }
